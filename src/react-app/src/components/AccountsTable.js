de52ff37d1c228b65c8fc773ecf4b9a47fb830df
Mohammad Atallah <m.atallah@beno.com>
2021-07-09 21:17:33 +0400

    Enable filtering

--- src/react-app/src/components/AccountsTable.js
+++ src/react-app/src/components/AccountsTable.js
@@ -6,16 +6,36 @@ import { getAccounts } from '../api'
 function AccountsTable() {
 
 	const [accounts, setAccounts] = React.useState([]);
+	const [filteredAccounts, setFilteredAccounts] = React.useState([]);
 	const tableRef = React.createRef();
 
-	React.useEffect(() => {
+	const fetchAccounts = () => {
 		getAccounts()
 			.then((response) => {
 				setAccounts(response.data.data);
+				setFilteredAccounts(response.data.data);
 			})
 			.catch((error) => {
-				console.log(error);
+				console.error(error);
 			});
+	}
+
+	const updateFilteredAccounts = (predicate, data) => {
+		let tmp = filteredAccounts;
+		if (predicate) {
+			if (!tmp.includes(data)) {
+				tmp.push(data);
+			}
+		} else {
+			if (tmp.includes(data)) {
+				tmp.splice(tmp.indexOf(data), 1);
+			}
+		}
+		setFilteredAccounts(tmp);
+	}
+
+	React.useEffect(() => {
+		fetchAccounts();
 	}, []);
 
 	return (
@@ -28,15 +48,31 @@ function AccountsTable() {
 				title="Accounts"
 				tableRef={tableRef}
 				options={{
+					filtering: true,
 					paging: false,
 					search: false
 				}}
 				columns={[
-					{ title: 'ID', field: '_id' },
-					{ title: 'Balance', field: 'balance' },
-					{ title: 'Created At', field: 'createdAt' },
-					{ title: 'Updated At', field: 'updatedAt' },
-					{ title: 'Status', field: 'status' },
+					{ title: 'ID', field: '_id', filtering: false },
+					{ title: 'Balance', field: 'balance', filtering: false },
+					{ title: 'Created At', field: 'createdAt', filtering: false },
+					{ title: 'Updated At', field: 'updatedAt', filtering: false },
+					{
+						title: 'Status',
+						field: 'status',
+						lookup: {
+							'pending': 'Pending',
+							'approved': 'Approved',
+							'funded': 'Funded',
+							'closed': 'Closed',
+							'suspended': 'Suspended',
+						},
+						customFilterAndSearch: (term, data) => {
+							const predicate = term.length ? term.includes(data.status) : true;
+							updateFilteredAccounts(predicate, data);
+							return predicate;
+						}
+					},
 				]}
 				data={accounts}
 				actions={[
@@ -44,7 +80,7 @@ function AccountsTable() {
 						icon: 'refresh',
 						tooltip: 'Refresh Data',
 						isFreeAction: true,
-						onClick: () => tableRef.current && tableRef.current.onQueryChange(),
+						onClick: () => fetchAccounts(),
 					}
 				]}
 				components={{
@@ -52,8 +88,8 @@ function AccountsTable() {
 						<div>
 							<MTableToolbar {...props} />
 							<div className="toolbar">
-								<p>Total # of accounts: {accounts.length}</p>
-								<p>Total balance: {accounts.length ? accounts.reduce((a, b) => a + (b['balance'] || 0), 0) : 0}</p>
+								<p>Total # of accounts: {filteredAccounts.length}</p>
+								<p>Total balance: {filteredAccounts.length ? filteredAccounts.reduce((a, b) => a + (b['balance'] || 0), 0) : 0}</p>
 							</div>
 						</div>
 					),
