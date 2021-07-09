a074513fbf635e69be49c3e5077906cf767a712f
Mohammad Atallah <m.atallah@beno.com>
2021-07-09 20:35:40 +0400

    Implement PATCH /accounts/{id}

--- src/index.js
+++ src/index.js
@@ -26,6 +26,43 @@ app.get("/api/accounts", (req, res) => {
 	});
 });
 
+app.patch('/api/accounts/:id', async (req, res, next) => {
+	const account = await models.Account.findById(req.params.id).exec();
+	if (!account) {
+		return res.status(404).send('The account with the given ID was not found.');
+	}
+	let query = { $set: {} };
+	for (let key in req.body) {
+		if (account[key] && account[key] !== req.body[key])
+			query.$set[key] = req.body[key];
+	}
+	if (query.$set && query.$set.status) {
+		switch (query.$set.status) {
+			case 'approved':
+				if (account.status !== 'pending') {
+					res.sendStatus(403);
+					return;
+				}
+				break;
+			case 'funded':
+				if (account.status !== 'approved') {
+					res.sendStatus(403);
+					return;
+				}
+				break;
+			case 'closed':
+				break;
+			case 'suspended':
+				break;
+			default:
+				res.status(422).send('Status is invalid');
+				break;
+		}
+	}
+	await models.Account.updateOne({ _id: req.params.id }, query).exec();
+	res.sendStatus(200);
+});
+
 connectDb().then(async () => {
 
 	app.listen(process.env.PORT, () =>
